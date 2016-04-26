26f10433c09e22d8e8506682d9ea2bac02dbb264
Dominik Knafelj <d.knafelj@gmail.com>
2016-04-25 21:27:17 -0400

    init

---
+++ src/durchschnitt/Durchschnitt.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package durchschnitt;
+
+/**
+ *
+ * @author Dominik Knafelj <d.knafelj@gmail.com>
+ */
+public class Durchschnitt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
